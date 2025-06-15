45de47fcc6fc84a22d340aa08028ac165f62c426
Muhammad-Ahmad17 <163166702+Muhammad-Ahmad17@users.noreply.github.com>
2025-06-16 01:03:50 +0500

    deployment info updation

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from '@/components/ui/use-toast';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
 const api = axios.create({
