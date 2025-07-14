98db8053131440fb011e89030d6e9d24ad846a6a
Muhammad-Ahmad17 <163166702+Muhammad-Ahmad17@users.noreply.github.com>
2025-07-14 12:46:05 +0500

    .

--- server/routes/registrationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const registrationController = require('../controllers/registrationController');
-
-router.post('/register', registrationController.register);
-router.post('/login', registrationController.login);
-
-module.exports = router;
